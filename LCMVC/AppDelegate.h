51c63127df4f52933b726a8ab32c439a43a253f2
linchu-bwf <linchu@51testing.com>
2019-01-23 14:32:00 +0800

    Initial Commit

---
+++ LCMVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCMVC
+//
+//  Created by linchu on 2019/1/23.
+//  Copyright © 2019 linchu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCMVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCMVC
+//
+//  Created by linchu on 2019/1/23.
+//  Copyright © 2019 linchu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
